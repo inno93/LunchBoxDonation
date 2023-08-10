13c0dbaa41bd008e50480f675b2622bc3b473589
inno93 <hih890400@gmail.com>
2023-08-10 22:48:55 +0900

    Revert "lunchbox 백앤드 작업"
    This reverts commit f073fbde0f2154e7000fb223a59a059e888e1ef6.

--- LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/admin/BoLunchBoxController.java
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/controller/admin/BoLunchBoxController.java
@@ -1,19 +1,14 @@
 package com.lunchbox.lunchboxdonation.controller.admin;
 
-import com.lunchbox.lunchboxdonation.service.lunchbox.LunchBoxService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
-@RequiredArgsConstructor
 @RequestMapping("admin")
 public class BoLunchBoxController {
-    private final LunchBoxService lunchBoxService;
 
     //    기부
     @GetMapping("donation")
@@ -32,10 +27,7 @@ public class BoLunchBoxController {
 //    상세 보기
     @GetMapping("lunchboxDetail")
     public void lunchBoxDetail(){
-//        ModelAndView mv = new ModelAndView();
-////        mv.setViewName("admin/lunchbox/lunchboxDetail");
-//        mv.setViewName("admin/lunchboxDetail");
-//        return mv;
+
     }
 
     // 이달의 특가
